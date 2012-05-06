2841092e25da46807e9a17a6f731ef4bc690df06
Hunter Barrington <hunter@barringtonweb.com>
2012-05-05 21:18:54 -0400

    removing rails in favor of nodejs

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-AnduinoCopterReporter::Application.config.secret_token = '1565123908ebbbad841f21249a15e71c5c3f33278826f7ec95bbd69d08d6454a59f944cfd6c85f1cd3877f5fa3a4ea5a83b0631bc81e5fba4864cfb7d3f9f447'
